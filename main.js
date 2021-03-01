0622ea9e8b42403a4c238b1c5bc292921d713c56
Giuseppe <festagiuseppe89@gmail.com>
2021-03-01 18:18:08 +0100

    esercizio completato + bonus da verificare

--- main.js
+++ main.js
@@ -14,14 +14,50 @@
 var numeriRandom = [];
 var numeriUtente = [];
 var punteggio = 0;
-var possibilita = 84;  //(100 - 16) volte
+var tentativi = 84;  //(100 - 16) volte
 var numeroUtente;
+var vietato = false;
+var numeroMax;
+var numeroMin;
+var livello
 
 
-// genero numeri random
+//BONUS da verificare funzionamento "da 1 a 100"
+// chiedo all'utente il livello di difficoltà tra 0, 1, 2
+livello = parseInt(prompt("Inserisci il livello di difficoltà: 0, 1 oppure 2"));
+while (livello != 0 && livello != 1 && livello != 2) {
+    livello = parseInt(prompt("Per favore inserisci il livello corretto di difficoltà: 0, 1 oppure 2"));
+}
+
+switch (livello) {
+    case 0:
+        numeroMin = 1;
+        numeroMax = 100;
+        possibilita = 84;
+    var titoloDomanda = "Inserisci un numero da 1 a 100";
+    break;
+    case 1:
+        numeroMin = 1;
+        numeroMax = 80;
+        possibilita = 64;
+        titoloDomanda = "Inserisci un numero da 1 a 80";
+    break;
+    case 2:
+        numeroMin = 1;
+        numeroMax = 50;
+        possibilita = 34;
+        titoloDomanda = "Inserisci un numero da 1 a 50";
+    break;
+}
+//BONUS
+
+
+
+
+// genero 16 numeri random per computer
 while (numeriRandom.length < 16) {
     //inserisco solo se il numero non è già presente nell'array
-    var numeroCasuale = generaNumeriRandom(1, numeroMax);
+    var numeroCasuale = generaNumeriRandom(1, 100);
     var cerca = presenteInArray(numeriRandom, numeroCasuale);
     if (cerca == false) {
     numeriRandom.push(numeroCasuale);
@@ -31,3 +67,66 @@ console.log(numeriRandom);
 document.getElementById("numeri-random").innerHTML = numeriRandom;
 
 //l'utente inserisce un numero per 84 tentativi
+while (numeriUtente.length < tentativi && vietato == false) {
+    
+    // chiedo un numero all'utente per verificare che i numeri rispettino il range
+    numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 100"));
+
+    if (presenteInArray(numeriUtente, numeroUtente) == false) {
+    numeriUtente.push(numeroUtente);
+      // se il numero dell'utente è gia presente hai perso
+    if (presenteInArray(numeriRandom, numeroUtente) == true) {
+        console.log("hai perso");
+        document.getElementById("messaggio").innerHTML = "Che peccato, hai perso. Hai beccato il numero nascosto.";
+        vietato = true;
+    } else {
+        punteggio++;
+    }
+}
+}
+document.getElementById("punteggio").innerHTML = "Hai totalizzato un punteggio di " + punteggio + " punti!";
+
+//da verificare il giusto funzionamento
+if (numeriUtente.length == tentativi) {
+    document.getElementById("messaggio").innerHTML = "Hai vinto!";
+    console.log("hai vinto");
+}
+//////////////////////
+
+// funzione che genera numero random
+function generaNumeriRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+ // funzione controlla che un numero sia in un range
+function generaNumeriRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// funzione cerca in array
+function presenteInArray(array, element) {
+    var i = 0;
+    var result = false;
+    while (i < array.length && result == false) {
+    if (array[i] == element) {
+        result = true;
+    }
+    i++;
+    }
+    return result;
+}
+
+// funzione che richiede un numero corretto
+function richiediNumeroCorretto() {
+    while (controlloRangeNumeri(numeroMin, numeroMax, numeroUtente) == false) {
+    numeroUtente = parseInt(prompt("Per favore inserisci un numero corretto: da 1  a " + numeroMax));
+    console.log('Numero inserito: ' + numeroUtente);
+    }
+}
+
+//funzione controlla che non sia inserito lo stesso numero pu di una volta
+function numUgualeInserito() {
+    while (presenteInArray(numeriUtente, numeroUtente) == true) {
+    numeroUtente = parseInt(prompt ("Avevi già inserito questo numero. Riprova con un numero diverso da " + numeroMin + " a " + numeroMax));
+    }
+}
